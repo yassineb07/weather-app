0ba73d482bfaeac4e6151005c8d9b1a919960cf9
yassine b <progyss9@gmail.com>
2023-08-08 22:35:22 +0100

    Get information from weather api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+
+const getWeather = async (location) => {
+  const baseURL = 'http://api.weatherapi.com/v1/current.json?key=';
+  const KEY = 'f7cde5d220a0466bba9165937232807';
+  const URL = `${baseURL}${KEY}&q=${location}`;
+
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+};
+
+getWeather('paris');
