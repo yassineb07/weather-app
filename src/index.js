f980188bdae9e9882f128725533c832808900fc2
yassine b <progyss9@gmail.com>
2023-08-08 23:06:25 +0100

    Add ability to get input from user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 
-const getWeather = async (location) => {
+const getWeatherInfo = async (location) => {
   const baseURL = 'http://api.weatherapi.com/v1/current.json?key=';
   const KEY = 'f7cde5d220a0466bba9165937232807';
   const URL = `${baseURL}${KEY}&q=${location}`;
@@ -10,4 +10,16 @@ const getWeather = async (location) => {
   console.log(data);
 };
 
-getWeather('paris');
+const getUserInput = () => {
+  const location = document.getElementById('location').value;
+  return location;
+};
+
+const form = document.getElementById('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = getUserInput();
+  if (!location) return;
+  console.log(location);
+  getWeatherInfo(location);
+});
